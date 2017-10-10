299f55b06ba097e8e4f5123bad184c0dfd8a2a40
Wirkmood <wirkmood@gmail.com>
2017-10-10 19:16:27 +0200

    Update Create_Security_group.ps1

--- Create_Security_group.ps1
+++ Create_Security_group.ps1
@@ -2,11 +2,31 @@
 #             Sécurity group             #
 ##########################################
 
+try {
+    # Login on Microsoft Azure
+    $Subscription = Get-AzureRmSubscription -ErrorAction:0
+}
+catch {
+    Write-Host -f Red "you're not connected on Microsoft Azure"
+}
+
+
+if ($Subscription -eq $null){
+    Login-AzureRmAccount
+    $Subscription = Get-AzureRmSubscription
+}
+else {
+    Write-Host -ForegroundColor Green "You're already connected on Microsoft Azure"
+}
 
 # Cloud variables
 $RM = Get-AzureRmResourceGroup 
 $loc = Get-AzureRmLocation | where {$_.Location -imatch "westeurope"}
 $StorageAccount = "yourStorageAccountName"
+
+###############################
+# Create a new Security group
+###############################
 
 # Create an inbound network security group rule for port 80
 $nsgRuleWeb = New-AzureRmNetworkSecurityRuleConfig -Name SecurityGroupHTTP  -Protocol Tcp `
